a144f4f363fdedec8ac1c1d49d89f46595d215b3
LiamG. <l530g592@ku.edu>
2025-09-19 17:40:43 -0500

    Add files via upload

---
+++ Lab4/cfile/isOdd.c
@@ -0,0 +1,10 @@
+#include "isOdd.h"
+#include <stdio.h>
+
+int isOdd(int n)
+{
+	if (n % 2 == 1){
+		 printf("%d is odd.\n", n);
+	}
+	return 0;
+}
---
+++ Lab4/cfile/main.c
@@ -0,0 +1,10 @@
+#include "isOdd.h"
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    isOdd(n);
+    return 0;
+}
